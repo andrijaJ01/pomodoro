cc7bf4d93ca8e994c8fe7cb09df96350c505dd3b
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-02-02 11:42:27 +0100

    class comments update

--- pomodoro.py
+++ pomodoro.py
@@ -9,18 +9,32 @@ class Pomodoro:
     '''
     cycle=0
     def __init__(self,work_time,rest_time):
+        """
+        constructor for pomodoro
+        takes 2 args
+        work duration period
+        and resting period
+        """
         self.work=work_time   #work time in minutes
         self.rest=rest_time   #rest time in minutes
         self.work_sec= self.work * 60
         self.rest_sec= self.rest * 60
 
     def _takerest(self):
+
+        """
+        internal function of the class, takes resting cycle
+        """
         for s in range(self.work_sec):
             print(f'Resting cycle No. {Pomodoro.cycle+1}')
             print(s)
             time.sleep(1)
             os.system('clear')           
     def run_timer(self):
+        """
+        starts the timer and 
+        automatically takes rests.
+        """
         os.system('clear')
         
         while Pomodoro.cycle <=3:
