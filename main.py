9ecf5fff5eb6f68d1f03e499e60af7863784a0c1
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-02-02 07:23:38 +0100

    dynamic timer in cli

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ class Pomodoro:
         self.work=work_time   #work time in minutes
         self.rest=rest_time   #rest time in minutes
         self.work_sec= self.work * 60
+        self.rest_sec= self.rest * 60
 
+    def _takerest(self):
+        for s in range(self.work_sec):
+            print(f'Resting cycle No. {Pomodoro.current_pom+1}')
+            print(s)
+            time.sleep(1)
+            os.system('clear')           
     def run_timer(self):
         os.system('clear')
         
@@ -21,8 +28,10 @@ class Pomodoro:
                 print(f'Cycle no. {Pomodoro.current_pom+1}')
                 print(s)
                 time.sleep(1)
-                os.system('clear')           
+                os.system('clear')   
+            self._takerest()        
             Pomodoro.current_pom+=1
+
         print('Pomodoro cant run anymore')
 
 timer=Pomodoro(1,5)
